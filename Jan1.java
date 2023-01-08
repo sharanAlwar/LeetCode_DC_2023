41a4dd31baccc0f2d7c2bf909fc269e403b881c8
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-08 12:29:19 +0530

    Create Jan1.java

---
+++ Jan1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void main(String[] args) {
+        //HashMap<Character,String> hash = new HashMap();
+        System.out.println(wordPattern("abba","dog dog dog dog"));
+
+    }
+    public static boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        if(pattern.length()!=arr.length){
+            return false;
+        }
+        //abba
+        //cat fish fish cat
+        HashMap<Character,String> hash = new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            if(hash.containsValue(arr[i]) && !Objects.equals(hash.get(pattern.charAt(i)), arr[i])){
+                return false;
+            }
+            if(hash.containsKey(pattern.charAt(i)) && !Objects.equals(hash.get(pattern.charAt(i)), arr[i])) {
+                return false;
+            }
+            hash.put(pattern.charAt(i),arr[i]);
+        }
+        return true;
+    }
+}
