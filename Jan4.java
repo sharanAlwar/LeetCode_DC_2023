40088fc84f188c9f4a202871771631a584dd7b5a
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-08 12:31:04 +0530

    Create Jan4.java

---
+++ Jan4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        Arrays.sort(tasks);
+        int min=0;
+        HashMap<Integer,Integer> a= new HashMap<>();
+        for(int i=0;i<tasks.length;i++)
+        {
+            a.put(tasks[i],a.getOrDefault(tasks[i],0)+1);
+        }
+
+        for(int i:a.values())
+        {
+            if(i==1)
+            return -1;
+            else
+            min=min+(i+2)/3;
+
+        }
+        return min;
+    }
+}
