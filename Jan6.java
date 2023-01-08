312c8805e0fb544c4b95877487f7d9789cb4c9f2
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-08 12:33:10 +0530

    Create Jan6.java

---
+++ Jan6.java
@@ -0,0 +1,14 @@
+//Birthday DC 
+//Looks like LeetCode is giving me IceCreem on my Bday!!
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int n = costs.length;
+        int answer = 0;
+        while (answer < n && costs[answer] <= coins) { 
+            coins -= costs[answer];
+            answer += 1;
+        }
+        return answer;
+    }
+}
