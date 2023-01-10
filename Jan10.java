8603c561bcf5c95110fae6ee077b65bc0c74e5b9
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-01-10 19:44:25 +0530

    Create Jan10.java

---
+++ Jan10.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        ArrayList<Integer> al1 = new ArrayList<>();
+        ArrayList<Integer> al2 = new ArrayList<>();
+        helper(p,al1);
+        helper(q,al2);
+        return (al1.equals(al2))? true : false;
+    }
+	public static void helper(TreeNode root , ArrayList al) {
+		if(root!=null) {
+		al.add(root.val);
+		helper(root.left,al);
+		helper(root.right,al);
+        }
+        al.add(null);
+    }
+}
