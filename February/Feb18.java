393c88c30f8cc9a686bad876191814fa477aa562
sharanAlwar <93466740+sharanAlwar@users.noreply.github.com>
2023-02-25 13:12:40 +0530

    Create Feb18.java

---
+++ February/Feb18.java
@@ -0,0 +1,32 @@
+//226
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return helper(root);
+    }
+
+    public static TreeNode helper(TreeNode root){
+        if(root == null){
+            return null;
+        }
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        root.right = helper(left);
+        root.left = helper(right);
+        return root;
+    }
+}
